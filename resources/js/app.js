252c508647be2506872e9073713e30d74a641489
Ancy42 <ancymalu79@gmail.com>
2026-03-17 16:07:24 +0530

    first

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import IndexPage from './components/IndexPage.vue'
+
+// const app = createApp({})
+// app.component('index-page', IndexPage)
+// app.mount('#app')       
+////////<!-- The 1 line of code below is better than the three lines of code above. -->
+createApp(IndexPage).mount('#app')
---
+++ resources/js/components/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Hello I'm Ancy</h1>
+</template>
+
+<script>
+    export default{
+        name:"IndexPage"
+    }
+</script>
